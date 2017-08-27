7ff4788ab38adfc4c23a3d3634e11a1f73cbe6cb
miyakey <miyasaviola@gmail.com>
2017-08-27 20:38:42 +0000

    kaminariページング追加

--- demo/app/controllers/blogs_controller.rb
+++ demo/app/controllers/blogs_controller.rb
@@ -1,7 +1,7 @@
 class BlogsController < ApplicationController
   def index
-    #@blogs = Blog.page(params[:page]).per(3)
-    @blogs = Blog.all
+    @blogs = Blog.page(params[:page]).per(3)
+    #@blogs = Blog.all
   end
 
   def new
